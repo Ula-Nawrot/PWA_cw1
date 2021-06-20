cebba75163d2354bcab6cc8577d06f59272217e6
UlaNawrot <ula.nawrot@gmail.com>
2021-06-20 10:57:04 +0200

    zmiana5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const cacheName = "podyplomowe";
-const filesToCache = ["./", "./ondex.html", "./css/style.css", "./js/main.js"];
+const filesToCache = ["./", "./index.html", "./css/style.css", "./js/main.js"];
 
 /* Start the service worker and cache all of the app's content */
 self.addEventListener("install", function (event) {
